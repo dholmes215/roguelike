360c078b8513ee2053514897b4f2ca219768ce4d
David Holmes <dholmes@dholmes.us>
2024-05-01 19:00:08 -0500

    Switch to 12px font I can actually read

--- src/main.rs
+++ src/main.rs
@@ -1918,7 +1918,7 @@ fn main() {
     tcod::system::set_fps(LIMIT_FPS);
 
     let root = Root::initializer()
-        .font("arial10x10.png", FontLayout::Tcod)
+        .font("arial12x12.png", FontLayout::Tcod)
         .font_type(FontType::Greyscale)
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
         .title("Rust/libtcod tutorial")
